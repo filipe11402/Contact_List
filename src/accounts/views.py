14d5b4493433541dc02ec92c79b4b0e5585288bc
filipe11402 <filipe11402@outlook.pt>
2021-06-16 11:59:50 +0100

    fixed static files + auth templates

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -7,3 +7,8 @@ accounts = Blueprint('accounts', __name__)
 @accounts.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('accounts/register.html')
+
+
+@accounts.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('accounts/login.html')
